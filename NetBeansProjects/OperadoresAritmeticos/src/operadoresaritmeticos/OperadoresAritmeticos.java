0f2fc554e1c07585bea2425ce6e88ae3139d21dd
Fernanda Maresi <ferferborges@gmail.com>
2020-07-25 23:27:25 -0300

    Create OperadoresAritmeticos.java

---
+++ NetBeansProjects/OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author ferfe
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //media
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        //System.out.println("A média é igual a "+ m);
+        
+        //++ pos incremento
+        int numeroa = 5;
+        numeroa++;
+       // System.out.println(numeroa);
+        
+        //-- pos decremento
+        int numerob = 5;
+        numerob--;
+        //System.out.println(numerob);
+        
+        // pré incremento
+        int numeroc = 5;
+        int valor = 5 + ++numeroc;
+       // System.out.println(valor);
+       // System.out.println(numeroc);
+        
+        //a = a+1
+        int numerod = 5;
+        numerod++;
+        System.out.println(numerod);*/
+        
+        //a = a-1
+        /*int numeroe = 5;
+        numeroe--;
+        //System.out.println(numeroe);
+        
+        int numerof = 5;
+        int valor = 5 + numerof++;
+       
+        System.out.println(valor);
+        System.out.println(numerof);
+        
+        // a = 6 + 5
+        int numerog = 5;
+        int valora = 5 + ++numerog;
+        System.out.println(valora);
+        */
+        
+        /*int numeroh = 10;
+        int valorb = 4 + numeroh--;
+        System.out.println(valorb);
+        System.out.println(numeroh);*/
+        
+        /*Pre-incremento a= a+1   ++a
+          Pós-incremento a= a+1   a++
+          
+          Pré-decremento a= a-1   --a
+          Pós-decremento a= a-1   a--
+        
+        += Somar e atribuir       a += b   a = a+b
+        -= Subtrair e atribuir    a -= b    a = a - b
+        *= Multiplicar e atribuir a *= b    a = a * b
+        /= Dividir e atribuir     a /= b    a = a / b
+        %= Resto e atribuir       a %= b    a = a % b
+        */
+        
+        //soma
+        int x = 4;
+        x += 2; //x = x +2
+        System.out.println(x);
+        
+        //subtracao
+        int xa = 4;
+        xa -= 2; //x = x - 2
+        System.out.println(xa);
+        
+        //subtracao
+        int xb = 4;
+        xb *= 2; //x = x * 2
+        System.out.println(xb);
+        
+        //divisao
+        int xc = 4;
+        xc /= 2; //x = x / 2
+        System.out.println(xc);
+        
+        //divisao
+        int xd = 4;
+        xd %= 2; //x = x % 2
+        System.out.println(xd);
+        
+        
+        
+    }
+    
+}
